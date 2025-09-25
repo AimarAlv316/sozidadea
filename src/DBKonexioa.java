458371200bbb43008df90dfd0a96c95f4a69aaf7
Aimar Alvarez <aalvarez24@izarraitz.eus>
2025-09-25 09:54:54 +0200

    DBKonexioa hasi

---
+++ src/DBKonexioa.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBKonexioa {
+    private static final String URL = "";
+    private static final String erabiltzailea = "root";
+    private static final String pasahitza = "root";
+
+    public static Connection getConexion() throws SQLException {
+        return DriverManager.getConnection(URL, erabiltzailea, pasahitza);
+    }
+}
